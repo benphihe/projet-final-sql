acb681ea396934a3f3a0562e9fab7011c7f00ec4
benphihe <150243312+benphihe@users.noreply.github.com>
2024-10-24 14:32:12 +0200

    modification du formulaire

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/insert', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'form.php'));
+    res.sendFile(path.join(__dirname, 'public', 'insert.html'));
 });
 app.get('/employees.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'employees.html'));
