928b0fcc04c20a353b11fda8e82116923ca58434
benphihe <150243312+benphihe@users.noreply.github.com>
2024-10-24 13:42:18 +0200

    creation de la page insert

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('/insert', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'insert.html'));
+});
 app.get('/employees.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'employees.html'));
 });
